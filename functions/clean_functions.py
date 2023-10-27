aa80d34f079be8bdc7edfe1f8d68487c76593a62
Joaquin Villaverde Roldan <joaquin@MacBook-Air-de-Joaquin.local>
2023-10-27 20:38:00 +0200

    new push

--- functions/clean_functions.py
+++ functions/clean_functions.py
@@ -4,27 +4,36 @@
 def trailer(x):
     x = x.lower()
     if 'trailer' in x:
-        return True
+        return 1
     else:
-        return False
+        return 0
     
 def commentaries(x):
     x = x.lower()
     if 'commenta' in x:
-        return True
+        return 1
     else:
-        return False
+        return 0
     
 def deleted(x):
     x = x.lower()
     if 'deleted' in x:
-        return True
+        return 1
     else:
-        return False
+        return 0
     
 def behind(x):
     x = x.lower()
     if 'behind' in x:
-        return True
+        return 1
     else:
-        return False
+        return 0
+
+# La siguiente función recive un string, el cual necesitamos acortar. Para ello convertimos la frase a una lista.
+# A continuación retiramos el excedente no deseado y reunimos el string. Devolvemos una frasee más corta.
+
+def size(x):
+    x = x.split()
+    x = x[1:9]
+    x = " ".join(x)
+    return x
